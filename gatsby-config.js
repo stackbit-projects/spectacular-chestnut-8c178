715817b1fc0e95e3113f1dcdae06aac7f1496009
Stackbit Code Editor <projects@stackbit.com>
2021-06-15 15:09:30 +0000

    gatsby-config.js: changed

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     siteMetadata: {
         title: `Cosigned Communications`,
-        name: `Narative`,
+        name: `Cosigned.`,
         siteUrl: `https://gatsby-theme-novela.netlify.com`,
         description: `This is my description that will be used in the meta tags and important for search results`,
 
